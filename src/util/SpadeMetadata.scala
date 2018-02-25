dbf016767a52df11ad4a85f94c8367a677ff42ef
Yaqi Zhang <hsbzyq@gmail.com>
2018-02-24 17:11:46 -0800

    move spade metadata to prism metadata

--- src/util/SpadeMetadata.scala
+++ src/util/SpadeMetadata.scala
@@ -3,44 +3,24 @@ package spade.util
 import spade._
 import spade.node._
 
-import pirc.util._
-import pirc.collection.mutable._
+import prism.util._
+import prism.collection.mutable._
 
-trait SpadeMetadata extends Metadata { self:Spade =>
+trait SpadeMetadata extends Metadata {
 
   /* Coordinate of a spade node. Used for pisa and dot codegen */
-  object coordOf extends MOneToOneMap with MetadataMaps { 
-    type K = Node
-    type V = (Int, Int)
-    override def reset = {} // Set during graph construction
-  }
+  object coordOf extends OneToOneMap[Node, (Int,Int)] with MetadataMap
   
   /* Index of a spade node. Used for pisa codegen */
-  object indexOf extends MOneToOneMap with MetadataMaps {
-    type K = Node
-    type V = Int
-    override def reset = {} // Set during graph construction
-  }
+  object indexOf extends OneToOneMap[Node, Int] with MetadataMap
   
   /* Can a counter be the inner most counter in a chain */
-  object isInnerCounter extends MOneToOneMap with MetadataMaps {
-    type K = Node
-    type V = Boolean 
-    override def reset = {} // Set during graph construction
-  }
+  object isInnerCounter extends OneToOneMap[Node, Boolean] with MetadataMap
 
   /* GridNetork of GlobalIO */
-  object networkOf extends MOneToOneMap with MetadataMaps {
-    type K = GlobalIO[_<:PortType, _] 
-    type V = GridNetwork
-    override def reset = {} // Set during graph construction
-  }
+  object networkOf extends OneToOneMap[GlobalIO[_<:PortType, _], GridNetwork] with MetadataMap
 
   /* Name of nodes */
-  object nameOf extends MOneToOneMap with MetadataMaps {
-    type K = Node
-    type V = String 
-    override def reset = {} // Set during graph construction
-  }
+  object nameOf extends OneToOneMap[Node, String] with MetadataMap
 
 }
