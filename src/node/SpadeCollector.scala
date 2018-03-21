017e69cd231d70da30e32fd8dabfe4a6d9a5320d
Yaqi Zhang <hsbzyq@gmail.com>
2018-03-21 13:53:51 -0700

    performance optimization to spade collector

--- src/node/SpadeCollector.scala
+++ src/node/SpadeCollector.scala
@@ -9,10 +9,16 @@ import prism.traversal._
 trait SpadeCollector extends GraphCollector[SpadeNode] { self:SpadeNode =>
 
   override def collectDown[M<:SpadeNode:ClassTag:TypeTag](depth:Int= -1, logger:Option[Logging]=None):List[M] = {
+    // Performance optimization
     typeOf[M] match {
-      case tag if tag <:< typeOf[Routable] => 
-        // Performance optimization
+      case tag if tag <:< typeOf[Routable] => // Single layer of hiearchy
         super.collectDown[Routable](depth, logger).collect { case m:M => m }
+      case tag if tag <:< typeOf[Bundle[_]] =>  // Shouldn't get submodule's bundle
+        def visitDown(n:N):List[N] = n.children.collect { case c:M => c }
+        super.collect[M](visitDown _, depth, logger)
+      case tag if tag <:< typeOf[Module] => // No need to go inside the bundles
+        def visitDown(n:N):List[N] = n.children.filterNot { _.isInstanceOf[Bundle[_]] }
+        super.collect[M](visitDown _, depth, logger)
       case _ => super.collectDown[M](depth, logger)
     }
   }
