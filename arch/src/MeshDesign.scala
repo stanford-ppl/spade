63c72d0b7fd46672c3fe145e5e1fa345e7a92f55
Yaqi Zhang <hsbzyq@gmail.com>
2018-05-09 16:14:19 +0800

    add 16x8 design

--- arch/src/MeshDesign.scala
+++ arch/src/MeshDesign.scala
@@ -18,6 +18,7 @@ class StaticMeshCB(numRows:Int=2, numCols:Int=2, numArgIns:Int=3, numArgOuts:Int
 
 object SMeshCB2x2 extends StaticMeshCB(numRows=2, numCols=2, numArgIns=3, numArgOuts=3)
 object SMeshCB4x4 extends StaticMeshCB(numRows=4, numCols=4, numArgIns=10, numArgOuts=3)
+object SMeshCB16x8 extends StaticMeshCB(numRows=16, numCols=8, numArgIns=10, numArgOuts=3)
 
 class DynamicMeshCB(numRows:Int=2, numCols:Int=2, numArgIns:Int=3, numArgOuts:Int=3) extends Spade {
   override def toString = s"SN${numRows}x${numCols}"
@@ -35,6 +36,7 @@ class DynamicMeshCB(numRows:Int=2, numCols:Int=2, numArgIns:Int=3, numArgOuts:In
 object DMeshCB2x2 extends DynamicMeshCB(numRows=2, numCols=2, numArgIns=3, numArgOuts=3)
 object DMeshCB4x2 extends DynamicMeshCB(numRows=4, numCols=2, numArgIns=3, numArgOuts=3)
 object DMeshCB4x4 extends DynamicMeshCB(numRows=4, numCols=4, numArgIns=10, numArgOuts=3)
+object DMeshCB16x8 extends DynamicMeshCB(numRows=16, numCols=8, numArgIns=10, numArgOuts=3)
 
 object MyDMesh extends Spade {
   /* Example of overriding memory size parameters */
