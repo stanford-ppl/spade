ef7e1f5421fc0b21940e2d65c0dcd9f087ef4b99
Yaqi Zhang <hsbzyq@gmail.com>
2018-05-14 16:58:24 +0800

    remove bundle group and coord calculation from patterns

--- src/params/Patterns.scala
+++ src/params/Patterns.scala
@@ -3,49 +3,28 @@ package node
                           
 sealed trait Pattern extends Parameter
 
-trait GridPattern extends Pattern {
-  val step = 2 
-}
-
-trait GridCentrolPattern extends GridPattern {
-  val switchParam:SwitchParam
-  def switchAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val coord = if (isDynamic(top)) (i,j) else (i*step, j*step)
-    BundleGroup(param=switchParam, coord=Some(coord))
-  }
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup
-  def cuCoord(i:Int, j:Int)(implicit top:MeshTop) = {
-    top match {
-      case top if isDynamic(top) => (i, j)
-      case top => (step/2 + i*step, step/2 + j*step)
-    }
-  }
-}
+trait GridPattern extends Pattern
 
 trait GridFringePattern extends GridPattern {
-  val mcParam:MCParam
   val argFringeParam:ArgFringeParam
-  def argBundle(implicit top:MeshTop):BundleGroup = {
-    import top.param._
-    top match {
-      case top:StaticMeshTop => BundleGroup(argFringeParam)(top)
-      case top:DynamicMeshTop => BundleGroup(argFringeParam, coord=Some((numCols/2, numRows)))(top)
-    }
-  }
-  def mcAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    import top.param._
-    val coord = top match {
-      case top:StaticMeshTop => if (i==0) (-step/2, j*step) else (numCols*step+step/2, j*step)
-      case top:DynamicMeshTop => if (i==0) (-1, j) else (numCols, j)
-    }
-    BundleGroup(mcParam, coord=Some(coord))
-  }
+  val mcParam:MCParam
 }
 
 case class MCOnly(
   argFringeParam:ArgFringeParam=ArgFringeParam(),
   mcParam:MCParam=MCParam()
 ) extends GridFringePattern
+
+case class MC_DramAG(
+  argFringeParam:ArgFringeParam=ArgFringeParam(),
+  mcParam:MCParam=MCParam(),
+  dagParam:DramAGParam=DramAGParam()
+) extends GridFringePattern
+
+trait GridCentrolPattern extends GridPattern {
+  val switchParam:SwitchParam
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop):CUParam
+}
 
 /*
  *
@@ -61,9 +40,8 @@ case class Checkerboard (
   pcuParam:PCUParam=PCUParam(),
   pmuParam:PMUParam=PMUParam()
 ) extends GridCentrolPattern {
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val param = if ((i+j) % 2 == 0) pcuParam else pmuParam 
-    BundleGroup(param=param, coord=Some(cuCoord(i,j)))
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop) = {
+    if ((i+j) % 2 == 0) pcuParam else pmuParam 
   }
 }
 /*
@@ -80,9 +58,8 @@ case class ColumnStrip (
   pcuParam:PCUParam=PCUParam(),
   pmuParam:PMUParam=PMUParam()
 ) extends GridCentrolPattern {
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val param = if (j % 2 == 0) pcuParam else pmuParam 
-    BundleGroup(param=param, coord=Some(cuCoord(i,j)))
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop) = {
+    if (j % 2 == 0) pcuParam else pmuParam 
   }
 }
 /*
@@ -99,9 +76,8 @@ case class RowStrip (
   pcuParam:PCUParam=PCUParam(),
   pmuParam:PMUParam=PMUParam()
 ) extends GridCentrolPattern {
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val param = if (i % 2 == 0) pcuParam else pmuParam 
-    BundleGroup(param=param, coord=Some(cuCoord(i,j)))
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop) = {
+    if (i % 2 == 0) pcuParam else pmuParam 
   }
 }
 /*
@@ -119,11 +95,10 @@ case class MixAll (
   pmuParam:PMUParam=PMUParam(),
   scuParam:SCUParam=SCUParam()
 ) extends GridCentrolPattern {
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val param = if (i % 2 == 0) {
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop) = {
+    if (i % 2 == 0) {
       if (j % 2 == 0) pcuParam else pmuParam
     } else scuParam
-    BundleGroup(param=param, coord=Some(cuCoord(i,j)))
   }
 }
 /*
@@ -141,9 +116,8 @@ case class HalfAndHalf (
   pmuParam:PMUParam=PMUParam(),
   scuParam:SCUParam=SCUParam()
 ) extends GridCentrolPattern {
-  def cuAt(i:Int, j:Int)(implicit top:MeshTop):BundleGroup = {
-    val param = if (i % 2 == 0) if (j % 2 == 0) pcuParam else pmuParam
-                else if (j % 2 == 0) pmuParam else scuParam
-    BundleGroup(param=param, coord=Some(cuCoord(i,j)))
+  def cuAt(i:Int, j:Int)(implicit top:MeshTop) = {
+    if (i % 2 == 0) if (j % 2 == 0) pcuParam else pmuParam
+    else if (j % 2 == 0) pmuParam else scuParam
   }
 }
